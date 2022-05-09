e0494cd816777a0b5bdde659aa734fb5c0be5678
zzWeeZz <42831999+zzWeeZz@users.noreply.github.com>
2022-05-09 14:53:45 +0200

    Push

--- Snowflake/src/Snowflake/Snowflake.hpp
+++ Snowflake/src/Snowflake/Snowflake.hpp
@@ -18,13 +18,14 @@ namespace Snowflake
 	template<class Component>
 	class ComponentPool;
 
-	class Pool;
-
-	template<class TComponent>
-	uint32_t Hash()
+	
+	class IPool
 	{
-		return typeid(TComponent).hash_code();
-	}
+	public:
+		virtual void RegisterEntity(Entity entity) = 0;
+		virtual void DeRegisterEntity(Entity entity) = 0;
+		virtual bool IsEntityRegistered(Entity entity) = 0;
+	};
 
 	class Manager
 	{
@@ -41,14 +42,21 @@ namespace Snowflake
 			return entity;
 		}
 
+		// fix complexety to O(1)
 		bool DestroyEntity(Entity& entity)
 		{
 			if (entity == InvalidEntity) return false;
 			auto it = std::find(m_Entities.begin(), m_Entities.end(), entity);
 			if (it != m_Entities.end())
 			{
-				std::swap(*it, m_Entities.back());
+				if (m_Entities.size() > 1)
+					std::swap(*it, m_Entities.back());
 				m_Entities.pop_back();
+				while (!m_Registry[entity].empty())
+				{
+					static_cast<IPool*>(m_ComponentPools[m_Registry[entity].back()])->DeRegisterEntity(entity);
+					m_Registry[entity].pop_back();
+				}
 				entity = InvalidEntity;
 				return true;
 			}
@@ -67,23 +75,28 @@ namespace Snowflake
 		{
 			if (entity == InvalidEntity)
 			{
-				assert(false && "Invalid entity");
+				throw std::invalid_argument("AddComponent called with invalid entity.");
 			}
-			auto component = static_cast<ComponentPool<TComponent>*>(m_ComponentPools[typeid(TComponent).hash_code()]);
-			component->RegisterEntity(entity);
-			return component->GetComponent(entity);
+			auto& pool = MakeOrGetPool<TComponent>();
+
+			pool.RegisterEntity(entity);
+			m_Registry[entity].push_back(typeid(TComponent).hash_code());
+			return pool.GetComponent(entity);
 		}
 
-		// TODO: add support to check if the pool exists
 		template<class TComponent>
-		bool HasComponent(Entity entity)
+		TComponent* TryGetComponent(Entity entity)
 		{
 			if (entity == InvalidEntity)
 			{
-				assert(false && "Invalid entity");
+				throw std::invalid_argument("TryGetComponent called with invalid entity.");
 			}
-			auto& component = *static_cast<ComponentPool<TComponent>*>(m_ComponentPools[typeid(TComponent).hash_code()]);;
-			return component.IsEntityRegistered(entity);
+			auto& component = MakeOrGetPool<TComponent>();
+			if (HasComponent<TComponent>(entity))
+			{
+				return &component.GetComponent(entity);
+			}
+			return nullptr;
 		}
 
 		template<class TComponent>
@@ -91,9 +104,9 @@ namespace Snowflake
 		{
 			if (entity == InvalidEntity)
 			{
-				assert(false && "Invalid entity");
+				throw std::invalid_argument("GetComponent called with invalid entity.");
 			}
-			auto& component = *static_cast<ComponentPool<TComponent>*>(m_ComponentPools[typeid(TComponent).hash_code()]);
+			auto& component = MakeOrGetPool<TComponent>();
 			return component.GetComponent(entity);
 		}
 
@@ -102,32 +115,75 @@ namespace Snowflake
 		{
 			if (entity == InvalidEntity)
 			{
-				assert(false && "Invalid entity");
+				throw std::invalid_argument("RemoveComponent called with invalid entity.");
 			}
-			auto& component = *static_cast<ComponentPool<TComponent>*>(m_ComponentPools[typeid(TComponent).hash_code()]);;
-			component.DeRegisterEntity(entity);
+			auto& pool = MakeOrGetPool<TComponent>();
+			pool.DeRegisterEntity(entity);
+			auto& registry = m_Registry[entity];
+			auto it = std::find(registry.begin(), registry.end(), typeid(TComponent).hash_code());
+			if (it != registry.end())
+			{
+				registry.erase(it);
+			}
 		}
 
-		template<class TComponent>
-		bool CreateComponent()
+		template<typename TComponent>
+		bool HasComponent(Entity entity)
 		{
-			size_t hash = typeid(TComponent).hash_code();
-			m_ComponentPools[hash] = new ComponentPool<TComponent>();
-			return true;
+			if (entity == InvalidEntity)
+			{
+				throw std::invalid_argument("HasComponent called with invalid entity.");
+			}
+			auto& component = MakeOrGetPool<TComponent>();
+			return component.IsEntityRegistered(entity);
 		}
 
-		//TODO: Add Execute function.
-
-
+		template<typename ...TComponents>
+		bool HasComponents(Entity entity)
+		{
+			if (entity == InvalidEntity)
+			{
+				throw std::invalid_argument("HasComponents called with invalid entity.");
+			}
+			return (HasComponent<TComponents>(entity) && ...);
+		}
+		template<class ...TComponents, class TFunction>
+		void Execute(TFunction&& func)
+		{
+			for (auto entity : m_Entities)
+			{
+				if (HasComponents<TComponents...>(entity))
+				{
+					func(entity, GetComponent<TComponents>(entity)...);
+				}
+			}
+		}
 		static Manager& GetManager()
 		{
 			return s_Instance;
 		}
 	private:
 
+		template<class TComponent>
+		ComponentPool<TComponent>& MakeOrGetPool()
+		{
+			size_t hash = typeid(TComponent).hash_code();
+			auto it = m_ComponentPools.find(hash);
+			if (it == m_ComponentPools.end())
+			{
+				m_ComponentPools[hash] = new ComponentPool<TComponent>();
+			}
+			return *static_cast<ComponentPool<TComponent>*>(m_ComponentPools[hash]);
+		}
+		template<class TComponent>
+		ComponentPool<TComponent>& GetPoolWithHash(size_t hash)
+		{
+			return *static_cast<ComponentPool*>(m_ComponentPools[hash]);
+		}
+
 		static Snowflake::Manager s_Instance;
 		std::vector<Entity> m_Entities;
-
+		std::unordered_map<Entity, std::vector<size_t>> m_Registry;
 		std::unordered_map<size_t, void*> m_ComponentPools;
 
 	};
@@ -138,42 +194,40 @@ namespace Snowflake
 		return Manager::GetManager();
 	}
 
+	
 
 	template<class Component>
-	class ComponentPool
+	class ComponentPool : public IPool
 	{
 		friend Manager;
 	public:
 
-		void RegisterEntity(Entity entity)
+		void RegisterEntity(Entity entity) override
 		{
 			if (!m_RegisteredEntities[entity])
 			{
+				m_Components[entity] = Component();
 				m_RegisteredEntities[entity] = true;
 			}
 		}
 
-		void DeRegisterEntity(Entity entity)
+		void DeRegisterEntity(Entity entity) override
 		{
 			m_RegisteredEntities[entity] = false;
+			m_Components[entity] = Component();
 		}
 
-		bool IsEntityRegistered(Entity entity)
+		bool IsEntityRegistered(Entity entity)override
 		{
 			return m_RegisteredEntities[entity];
 		}
 
 		Component& GetComponent(Entity entity)
 		{
-			if (!m_RegisteredEntities[entity])
-			{
-				return Component();
-			}
 			return m_Components[entity];
 		}
 	private:
 		std::array<Component, 8192> m_Components = {};
 		std::bitset<8192> m_RegisteredEntities = { false };
 	};
-
 }
--- Tester/Tester.cpp
+++ Tester/Tester.cpp
@@ -6,6 +6,7 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace Tester
 {
+
 	TEST_CLASS(EntityManagment)
 	{
 	public:
@@ -20,6 +21,9 @@ namespace Tester
 			Assert::AreEqual(true, manager.ValidateEntity(entity1));
 			Assert::AreEqual(true, manager.ValidateEntity(entity2));
 			Assert::AreEqual(true, manager.ValidateEntity(entity3));
+			manager.DestroyEntity(entity1);
+			manager.DestroyEntity(entity2);
+			manager.DestroyEntity(entity3);
 		}
 
 		TEST_METHOD(EntityDestruction)
@@ -55,15 +59,8 @@ namespace Tester
 		};
 		TEST_METHOD(AddSingleComponent)
 		{
-			struct TransformComponent
-			{
-				float x = 0;
-				float y = 0;
-			};
-
 			auto& manager = Snowflake::Manager::GetManager();
 			Snowflake::Entity entity = manager.CreateEntity();
-			manager.CreateComponent<TransformComponent>();
 			manager.AddComponent<TransformComponent>(entity);
 			Assert::AreEqual(true, manager.HasComponent<TransformComponent>(entity));
 			manager.DestroyEntity(entity);
@@ -72,12 +69,8 @@ namespace Tester
 
 		TEST_METHOD(AddMultipleComponents)
 		{
-			
-
 			auto& manager = Snowflake::Manager::GetManager();
 			Snowflake::Entity entity = manager.CreateEntity();
-			manager.CreateComponent<TransformComponent>();
-			manager.CreateComponent<TestComponent>();
 			manager.AddComponent<TransformComponent>(entity);
 			manager.AddComponent<TestComponent>(entity);
 			Assert::AreEqual(true, manager.HasComponent<TransformComponent>(entity));
@@ -97,12 +90,32 @@ namespace Tester
 			manager.DestroyEntity(entity);
 		}
 
+		TEST_METHOD(RemoveComponent)
+		{
+			auto& manager = Snowflake::Manager::GetManager();
+			Snowflake::Entity entity = manager.CreateEntity();
+			manager.AddComponent<TransformComponent>(entity);
+			manager.AddComponent<TestComponent>(entity);
+			manager.RemoveComponent<TransformComponent>(entity);
+			Assert::AreEqual(false, manager.HasComponent<TransformComponent>(entity));
+			Assert::AreEqual(true, manager.HasComponent<TestComponent>(entity));
+			manager.DestroyEntity(entity);
+			
+			Snowflake::Entity entity2 = manager.CreateEntity();
+			manager.AddComponent<TransformComponent>(entity2);
+			manager.AddComponent<TestComponent>(entity2);
+
+			manager.RemoveComponent<TestComponent>(entity2);
+			Assert::AreEqual(true, manager.HasComponent<TransformComponent>(entity2));
+			Assert::AreEqual(false, manager.HasComponent<TestComponent>(entity2));
+			manager.DestroyEntity(entity2);
+			
+		}
+
 		TEST_METHOD(GetComponent)
 		{
 			auto& manager = Snowflake::GetManager();
 			Snowflake::Entity entity = manager.CreateEntity();
-			manager.CreateComponent<TransformComponent>();
-			manager.CreateComponent<TestComponent>();
 			manager.AddComponent<TransformComponent>(entity);
 			Assert::AreEqual(true, manager.HasComponent<TransformComponent>(entity));
 			Assert::AreEqual(0.f, manager.GetComponent<TransformComponent>(entity).x);
@@ -112,6 +125,7 @@ namespace Tester
 			Assert::AreEqual(true, manager.HasComponent<TestComponent>(entity));
 			Assert::AreEqual(0.f, manager.GetComponent<TransformComponent>(entity).x);
 			Assert::AreEqual(0.f, manager.GetComponent<TestComponent>(entity).a);
+			manager.DestroyEntity(entity);
 		}
 
 		TEST_METHOD(ChangeComponent)
@@ -119,25 +133,69 @@ namespace Tester
 			auto& manager = Snowflake::GetManager();
 			Snowflake::Entity entity = manager.CreateEntity();
 			Snowflake::Entity entity2 = manager.CreateEntity();
-			manager.CreateComponent<TransformComponent>();
-			manager.CreateComponent<TestComponent>();
 			auto& tfComponent = manager.AddComponent<TransformComponent>(entity);
 			Assert::AreEqual(true, manager.HasComponent<TransformComponent>(entity));
 			Assert::AreEqual(0.f, manager.GetComponent<TransformComponent>(entity).x);
 			tfComponent.x = 1.f;
 			auto& b = manager.GetComponent<TransformComponent>(entity);
 			Assert::AreEqual(1.f, b.x);
+			manager.DestroyEntity(entity);
+			manager.DestroyEntity(entity2);
 		}
-		/*TEST_METHOD(ExecuteFunction)
+		TEST_METHOD(ExecuteFunction)
 		{
 			auto& manager = Snowflake::GetManager();
+
+			{
+				Snowflake::Entity entity = manager.CreateEntity();
+				Snowflake::Entity entity2 = manager.CreateEntity();
+				Snowflake::Entity entity3 = manager.CreateEntity();
+				manager.AddComponent<TransformComponent>(entity);
+				manager.AddComponent<TransformComponent>(entity2);
+				manager.AddComponent<TransformComponent>(entity3);
+				manager.Execute<TransformComponent>([](Snowflake::Entity entity, TransformComponent& transform) {
+					transform.x = 1.f;
+					});
+				Assert::AreEqual(1.f, manager.GetComponent<TransformComponent>(entity).x);
+				Assert::AreEqual(1.f, manager.GetComponent<TransformComponent>(entity2).x);
+				Assert::AreEqual(1.f, manager.GetComponent<TransformComponent>(entity3).x);
+
+				manager.DestroyEntity(entity);
+				manager.DestroyEntity(entity2);
+				manager.DestroyEntity(entity3);
+			}
+
 			Snowflake::Entity entity = manager.CreateEntity();
-			manager.CreateComponent<TransformComponent>();
-			manager.AddComponent<TransformComponent>(entity);
-			manager.Execute<TransformComponent>([](Snowflake::Entity entity, TransformComponent& transform) {
-				transform.x = 1.f;
-			});
-			Assert::AreEqual(1.f, manager.GetComponent<TransformComponent>(entity).x);
-		}*/
+			Snowflake::Entity entity2 = manager.CreateEntity();
+			Snowflake::Entity entity3 = manager.CreateEntity();
+
+
+			manager.AddComponent<TransformComponent>(entity).x = 0;
+			manager.AddComponent<TransformComponent>(entity2).x = 0;
+			manager.AddComponent<TestComponent>(entity2);
+			manager.AddComponent<TransformComponent>(entity3).x = 0;
+
+			manager.Execute<TransformComponent, TestComponent>([](Snowflake::Entity entity, TransformComponent& transform, TestComponent& test) {
+				transform.x = 100.f;
+				test.a = 100.f;
+				});
+			Assert::AreEqual(100.f, manager.GetComponent<TransformComponent>(entity2).x);
+			Assert::AreEqual(100.f, manager.GetComponent<TestComponent>(entity2).a);
+
+			Assert::AreEqual(0.f, manager.GetComponent<TransformComponent>(entity).x);
+			Assert::AreEqual(0.f, manager.GetComponent<TransformComponent>(entity3).x);
+			Assert::IsNotNull(manager.TryGetComponent<TestComponent>(entity2));
+			manager.RemoveComponent<TestComponent>(entity2);
+			manager.Execute<TransformComponent, TestComponent>([](Snowflake::Entity entity, TransformComponent& transform, TestComponent& test) {
+				transform.x = 200.f;
+				test.a = 200.f;
+				});
+			Assert::AreEqual(100.f, manager.GetComponent<TransformComponent>(entity2).x);
+			Assert::IsNull(manager.TryGetComponent<TestComponent>(entity2));
+
+			Assert::AreEqual(0.f, manager.GetComponent<TransformComponent>(entity).x);
+			Assert::AreEqual(0.f, manager.GetComponent<TransformComponent>(entity3).x);
+		}
+
 	};
 }
